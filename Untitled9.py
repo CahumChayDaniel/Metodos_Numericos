8fe5155f41b27efbf70b1816eade9562b081eea4
CahumChayDaniel <100241170+CahumChayDaniel@users.noreply.github.com>
2022-02-22 15:53:07 -0600

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+print ("e^x")
+a = int(input("Ingrese el límite: "))
+
+def funcion_exponencial(x):
+    suma = 0
+    for n in range((a+1)):
+        suma += math.pow(x,n) / math.factorial(n)
+    return suma
+    resultado =suma
+
+b = float(input("Ingrese el valor de x: "))
+print("========================================================")
+r = 1/funcion_exponencial(b)
+print("Resultado con la función funcion_exponencial: ", funcion_exponencial(b))
+
+
+# In[ ]:
+
+
+
+
